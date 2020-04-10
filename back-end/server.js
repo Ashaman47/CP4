18a82cb1010756bf50c203a8b387271de435b830
Ashaman47 <59666448+Ashaman47@users.noreply.github.com>
2020-04-10 15:15:58 -0700

    Update server.js

--- back-end/server.js
+++ back-end/server.js
@@ -101,4 +101,4 @@ app.delete('/api/items/:id', async (req, res) => {
     res.sendStatus(500);
   }
 });
-app.listen(3000, () => console.log('Server listening on port 3000!'));
+app.listen(3001, () => console.log('Server listening on port 3001!'));
